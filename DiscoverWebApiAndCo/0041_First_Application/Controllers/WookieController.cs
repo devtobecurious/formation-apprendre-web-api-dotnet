385a4036a2390e09d5ede74cae927bc6efae8b8c
evan-boissonnot <commercial@boissonnot.Fr>
2019-10-29 00:00:16 +0100

    add two first projet

---
+++ DiscoverWebApiAndCo/0041_First_Application/Controllers/WookieController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _0041_First_Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]-[controller]")]
+    public class WookieController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            
+
+            return this.Ok(new { ok = "1" });
+        }
+    }
+}
