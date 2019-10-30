c7ff6d485a8848e11bc45e1b4ba838edf9af18f0
evanboissonnot <evan@boissonnot.fr>
2019-10-30 11:19:20 +0100

    Fin module 5

---
+++ DiscoverWebApiAndCo/0060_First_Application/Models/Contexts/DefaultContext.cs
@@ -0,0 +1,27 @@
+﻿
+namespace _0041_First_Application.Models.Contexts
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Transactions;
+
+    public class DefaultContext : DbContext
+    {
+        public DefaultContext() { }
+
+        public DefaultContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Wookie> Wookies { get; set; }
+    }
+}
