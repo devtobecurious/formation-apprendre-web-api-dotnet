c7fe172f28cdac555117fad163a3bfe9ddcea408
evan-boissonnot <commercial@boissonnot.Fr>
2019-11-02 13:01:01 +0100

    signi avec identity context

---
+++ DiscoverWebApiAndCo/0102_First_Application/Models/Contexts/DefaultContext.cs
@@ -0,0 +1,28 @@
+﻿
+namespace _0041_First_Application.Models.Contexts
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Transactions;
+
+    public class DefaultContext : IdentityDbContext
+    {
+        public DefaultContext() { }
+
+        public DefaultContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Wookie> Wookies { get; set; }
+    }
+}
