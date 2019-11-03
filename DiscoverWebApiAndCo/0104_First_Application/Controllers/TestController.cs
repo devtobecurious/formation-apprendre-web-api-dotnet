27d7771f1edf59a7d6ad852cc99f6f2f39ae0616
evan-boissonnot <commercial@boissonnot.Fr>
2019-11-03 10:29:18 +0100

    security end

---
+++ DiscoverWebApiAndCo/0104_First_Application/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _0102_First_Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        //[EnableCors("customOrigin2")]
+        [EnableCors("customOrigin")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok(new { isSuccess = true });
+        }
+
+        [EnableCors("customOrigin2")]
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return this.Ok(new { isSuccess = true });
+        }
+    }
+}
